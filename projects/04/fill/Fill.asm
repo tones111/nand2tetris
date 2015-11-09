ef969355155cf281f819bbe71581a9201c7925dc
Paul Sbarra <Sbarra.Paul@gmail.com>
2015-11-08 21:55:15 -0600

    Implement proj 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,46 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// SCREEN = 0x4000
+//    256 rows x 512 columns = 256 x 32 words (8192)
+//    0, 0 => Top Left
+//    Pixel = c%16 bit @(base + 32r + c/16)
+// KBD = 0x6000
+
+   @SCREEN
+   D=A
+   @ptr
+   M=D   // ptr = SCREEN
+
+   @8192
+   D=D+A
+   @eos
+   M=D   // end-of-screen(eos) = SCREEN + 8192 (256x32)
+
+(main)
+   @KBD
+   D=M
+   @fill
+   D; JEQ
+   D=-1
+
+(fill)
+   @ptr
+   A=M
+   M=D
+
+   @ptr
+   D=M+1
+   M=D     // ptr++
+
+   @eos
+   D=D-M
+   @main
+   D; JNE
+
+   @SCREEN
+   D=A
+   @ptr
+   M=D
+   @main
+   0; JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+   @R2
+   M=0
+
+   @R1   // fast-path if R1 == 0
+   D=M
+   @exit
+   D; JEQ
+
+   @R0   // fast-path if R0 == 0
+   D=M
+   @exit
+   D; JEQ
+
+(loop)      // do
+   @R1
+   D=M
+   @R2
+   M=D+M    // R2 += R1
+   @R0
+   MD=M-1   // while (R0 > 0)
+   @loop
+   D; JNE
+
+(exit)
+   @exit
+   0; JMP
